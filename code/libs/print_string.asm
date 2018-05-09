09d1a3fe3a009317ff38896d3697f8cf8bde2e97
TrisT <30376152+RealTrisT@users.noreply.github.com>
2018-05-09 15:51:36 +0100

    got to 32

--- code/libs/print_string.asm
+++ code/libs/print_string.asm
@@ -1,4 +1,4 @@
-
+[bits 16]
         ;16-bit real mode
 print_string_rm16:       ; Expects null terminated message in si
     mov al,[si]
@@ -17,3 +17,30 @@ print_char_rm16:
     mov bh,0x00         ; Page number.  Leave this alone.
     int 0x10            ; Signal video interrupt to BIOS
     retn
+
+
+[bits 32]
+    ; Define some constants
+VIDEO_MEMORY equ 0xb8000
+WHITE_ON_BLACK equ 0x0f
+    ; prints a null - terminated string pointed to by EDX
+print_string_pm:
+    pusha
+    mov edx, VIDEO_MEMORY           ; Set edx to the start of vid mem.
+
+print_string_pm_loop:
+    mov al, [ ebx ]                 ; Store the char at EBX in AL
+    mov ah, WHITE_ON_BLACK          ; Store the attributes in AH
+    cmp al, 0                       ; if (al == 0) , at end of string , so
+    je print_string_pm_done         ; jump to done
+    mov [edx], ax                   ; Store char and attributes at current
+                                    ; character cell.
+    add ebx, 1                      ; Increment EBX to the next char in string.
+    add edx, 2                      ; Move to next character cell in vid mem.
+    jmp print_string_pm_loop        ; loop around to print the next char.
+
+print_string_pm_done:
+    popa
+    ret                             ; Return from the function
+
+[bits 16]
--- code/main.asm
+++ code/main.asm
@@ -2,43 +2,60 @@
 [org 0x7C00]
 
 start:
-    mov [BOOT_DRIVE], dl
-    mov bp, 0x8000
+;    mov [BOOT_DRIVE], dl
+;    mov bp, 0x8000
+;    mov sp, bp
+
+;    mov si,MSG_booted
+;    call print_string_rm16
+
+;                        ; read
+;    mov dh, 0x5         ; 5 sectors
+;    ;mov es, 0          ; no segment
+;    mov bx, 0x9000      ; to 0x9000
+;    mov dl, [BOOT_DRIVE]; from this drive
+;    call disk_load
+;
+;    mov si, MSG_firstsect
+;    call print_string_rm16
+;    mov dx, [0x9000]    ; print
+;    call print_hex_rm16 ; first word of second sector
+;
+;    mov si, MSG_secndsect
+;    call print_string_rm16
+;    mov dx, [0x9200]    ; print
+;    call print_hex_rm16 ; first word of third sector
+
+
+    mov bp, 0x9000                  ; Set the stack.
     mov sp, bp
 
-    mov si,MSG_booted
+    mov si, MSG_booted
     call print_string_rm16
 
-                        ; read
-    mov dh, 0x5         ; 5 sectors
-    ;mov es, 0           ; no segment
-    mov bx, 0x9000      ; to 0x9000
-    mov dl, [BOOT_DRIVE]; from this drive
-    call disk_load
-
-    mov si, MSG_firstsect
-    call print_string_rm16
-    mov dx, [0x9000]    ; print
-    call print_hex_rm16 ; first word of second sector
-
-    mov si, MSG_secndsect
-    call print_string_rm16
-    mov dx, [0x9200]    ; print
-    call print_hex_rm16 ; first word of third sector
-
+    call switch_to_pm               ; Note that we never return from here.
 
     jmp $
 
 %include "libs/disk_read.asm"
 %include "libs/print_string.asm"
 %include "libs/print_hex.asm"
+%include "libs/rm16_to_pm32.asm"
+
+[bits 32]
+
+BEGIN_PM :
+    mov ebx , MSG_32Prot
+    call print_string_pm            ; Use our 32 - bit print routine.
+
+    jmp $                           ; Hang.
 
 ;data
     MSG_firstsect db 'First word from second section: ', 0x0
     MSG_secndsect db 0x0D, 0x0A, 'First word from third section: ', 0x0
 
-    MSG_booted db 'Booted',0x0D,0x0A,0
-
+    MSG_booted db 0x0A, 'Booted',0x0D,0x0A,0
+    MSG_32Prot db '32-BIT PROTECTED M0D3 BITCH', 0
     BOOT_DRIVE db 0x0
 
 
@@ -46,5 +63,5 @@ TIMES 510 - ($ - $$) db 0
 DW 0xAA55
 
 
-times 256 dw 0xdada
-times 256 dw 0xface
+;times 256 dw 0xdada
+;times 256 dw 0xface
